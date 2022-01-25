13f6e414dc991228bbf82280b2db9854ef1ccab7
Marlon Batisa <marlon.batista18@hotmail.com>
2022-01-25 08:54:58 -0300

    criando api rest C#

---
+++ api/UrnaEletronica/UrnaEletronica/Model/Candidato.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UrnaEletronica.Model
+{
+    public class Candidato
+    {
+        public string NomeCompleto { get; set; }
+        public string NomeVice { get; set; }
+        public DateTime DataDeRegistro { get; set; }
+        public int Legenda { get; set; }
+    }
+}
---
+++ api/UrnaEletronica/UrnaEletronica/Model/Voto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UrnaEletronica.Model
+{
+    public class Voto
+    {
+        public int IdCandidato { get; set; }
+        public DateTime DataDoVoto { get; set; }
+    }
+}
