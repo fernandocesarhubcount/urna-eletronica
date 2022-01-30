cb3938b0a6a6231221652d45c2143ce9f5218c61
Marlon Batisa <marlon.batista18@hotmail.com>
2022-01-30 03:21:26 -0300

    Adicionado classe VotosResultado

---
+++ api/UrnaEletronica/UrnaEletronica/Controllers/Commands/Outputs/VotosResultado.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UrnaEletronica.Controllers.Commands.Outputs
+{
+    public class VotosResultado
+    {
+        public int? Legenda { get; set; }
+        public String NomeCandidato { get; set; }
+        public int QtdVotos { get; set; }
+    }
+}
