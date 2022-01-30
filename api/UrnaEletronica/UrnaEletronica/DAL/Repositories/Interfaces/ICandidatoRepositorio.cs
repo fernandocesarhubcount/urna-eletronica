9e87f9d9f7ca67af0c58fe4665f15c0fb74ef9e2
Marlon Batisa <marlon.batista18@hotmail.com>
2022-01-30 03:13:14 -0300

    Adicionada interface para repositório Candidato

---
+++ api/UrnaEletronica/UrnaEletronica/DAL/Repositories/Interfaces/ICandidatoRepositorio.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UrnaEletronica.Controllers.Commands;
+using UrnaEletronica.Controllers.Commands.Outputs;
+using UrnaEletronica.Model;
+
+namespace UrnaEletronica.DAL.Repositories.Interfaces
+{
+    public interface ICandidatoRepositorio : IRepositorioEF<Candidato>
+    {
+        Candidato ObterPorLegenda(int legenda);
+    }
+}
