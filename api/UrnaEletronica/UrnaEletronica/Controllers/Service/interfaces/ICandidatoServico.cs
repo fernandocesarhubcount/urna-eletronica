ec6c963103cf6b60193266526c1e3c50195e0a25
Marlon Batisa <marlon.batista18@hotmail.com>
2022-01-30 03:20:43 -0300

    Adicionadas interfaces para serviço

---
+++ api/UrnaEletronica/UrnaEletronica/Controllers/Service/interfaces/ICandidatoServico.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UrnaEletronica.Controllers.Commands;
+using UrnaEletronica.DAL.Repositories.Interfaces;
+using UrnaEletronica.Model;
+using UrnaEletronica.Model.Inputs;
+
+namespace UrnaEletronica.Controllers.Service
+{
+    public interface ICandidatoServico
+    {
+        IComandoResultado SalvarCandidato(FormularioCandidato form);
+        IComandoResultado AlterarCandidato(int id, FormularioCandidato form);
+        bool LegendaJaRegistrada(int legenda);
+        Candidato ConverterFormParaCandidato(FormularioCandidato form);
+        IComandoResultado ApagarCandidato(int idCandidato);
+        IComandoResultado ApagarTodosCandidatos();
+    }
+}
---
+++ api/UrnaEletronica/UrnaEletronica/Controllers/Service/interfaces/IVotoServico.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UrnaEletronica.Controllers.Commands;
+using UrnaEletronica.Model;
+using UrnaEletronica.Model.Inputs;
+
+namespace UrnaEletronica.Controllers.Service
+{
+    public interface IVotoServico
+    {
+        IComandoResultado ObterTodosOsVotos();
+        IComandoResultado ObterTodosOsVotosCandidato(int legenda);
+        IComandoResultado SalvarVoto(FormVoto voto);
+    }
+}
