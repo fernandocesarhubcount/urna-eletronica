d3cea43a533dc3b74a0129974ff04b83263a6421
Marlon Batisa <marlon.batista18@hotmail.com>
2022-01-30 03:11:20 -0300

    Adicionada interface base para repositórios

---
+++ api/UrnaEletronica/UrnaEletronica/DAL/Repositories/Interfaces/IRepositorioEF.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UrnaEletronica.DAL.Repositories
+{
+    public interface IRepositorioEF<T> 
+    {
+        T ObterPorId(int Id);
+        IEnumerable<T> ObterTodos();
+        void Salvar(T obj);
+        void Alterar(T obj);
+        void Excluir(T obj);
+
+    }
+}
