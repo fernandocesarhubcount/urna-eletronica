d7bd0671c93e02ce479f6ecd7e071a211591e47d
Marlon Batisa <marlon.batista18@hotmail.com>
2022-01-30 03:15:52 -0300

    Adicionadas propriedades a classe Voto

--- api/UrnaEletronica/UrnaEletronica/Model/Voto.cs
+++ api/UrnaEletronica/UrnaEletronica/Model/Voto.cs
@@ -1,13 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
+using UrnaEletronica.DAL;
+using UrnaEletronica.DAL.Repositories;
+using UrnaEletronica.DAL.Repositories.Interfaces;
+using UrnaEletronica.Model.Inputs;
 
 namespace UrnaEletronica.Model
 {
     public class Voto
     {
-        public int IdCandidato { get; set; }
+        [Key]
+        public int Id { get; set; }
+        [ForeignKey("Candidato")]
+        public int? IdCandidato { get; set; }
+        public virtual Candidato  Candidato{ get; set; }
+        [Required]
         public DateTime DataDoVoto { get; set; }
+        [Required]
+        public bool VotoEmBranco { get; set; }
+
+        public Voto(FormVoto form)
+        {
+            IdCandidato = form.IdCandidato;
+            VotoEmBranco = form.VotoEmBranco;
+            DataDoVoto = DateTime.Now;
+        }
+
+        public Voto()
+        {
+
+        }
     }
 }
