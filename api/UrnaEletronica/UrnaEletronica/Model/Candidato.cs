821b3620ec4765e7d5c988265bbf3139ca26b236
Marlon Batisa <marlon.batista18@hotmail.com>
2022-01-30 03:15:21 -0300

    Adicionadas propriedades a classe Candidato

--- api/UrnaEletronica/UrnaEletronica/Model/Candidato.cs
+++ api/UrnaEletronica/UrnaEletronica/Model/Candidato.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,28 @@ namespace UrnaEletronica.Model
 {
     public class Candidato
     {
+        [Key]
+        public int IdCandidato { get; set; }
+        [Required]
+        public Int32 Legenda { get; set; }
+        [Required]
         public string NomeCompleto { get; set; }
+        [Required]
         public string NomeVice { get; set; }
+        [Required]
         public DateTime DataDeRegistro { get; set; }
-        public int Legenda { get; set; }
+
+        public Candidato(string nomeCompleto, string nomeVice, DateTime? dataRegistro, int legenda)
+        {
+            NomeCompleto = nomeCompleto;
+            NomeVice = nomeVice;
+            DataDeRegistro = (DateTime)(dataRegistro == null ? DateTime.Now : dataRegistro);       
+            Legenda = legenda;
+        }
+
+        public Candidato()
+        {
+
+        }
     }
 }
